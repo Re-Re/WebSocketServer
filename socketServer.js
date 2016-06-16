41ab20a46048bccf262c05ed5c0e3b5f6d9767ba
Re-Re <otsukijuan@gmail.com>
2016-06-16 15:50:05 +0800

    Update socketServer.js

--- socketServer.js
+++ socketServer.js
@@ -2,7 +2,9 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app);
     io = require('socket.io').listen(server);
-
+    
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.get('/',function(req , res) {
     res.sendFile(__dirname+'/index.html');
@@ -18,4 +20,4 @@ io.sockets.on('disconnect',function(socket){
     io.sockets.emit('message',"disconnected.");
 });
 
-server.listen(80);
+server.listen(server_port,server_ip_address);
